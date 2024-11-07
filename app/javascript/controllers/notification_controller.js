b21e16628052fb966cbd70ece2d0d71fc8d8e3d5
Lukas <lkoe@bluewin.ch>
2024-11-07 17:10:09 +0000

    Made notifications be rendered server-side if they exist

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -3,14 +3,6 @@ import $ from "jquery";
 
 // Connects to data-controller="notification"
 export default class extends Controller {
-    static targets = ["body"];
-
-    connect() {
-        if ($(this.bodyTarget).text()) {
-            $(this.element).removeClass("hidden");
-        }
-    }
-
     close() {
         $(this.element).addClass("hidden");
     }
