1f0fe907c134d5530d8ee0600ef36e5e8dfd2804
Lukas <lkoe@bluewin.ch>
2024-11-10 12:34:24 +0000

    Added tests to user model and controller

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,77 @@
+require "test_helper"
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  test "should get new" do
+    get new_user_registration_path
+    assert_response :success
+  end
+
+  test "should create new user" do
+    post user_registration_path, params: { user: { username: "abc", email: "abc@shadowcrafter.org", password: "PlaceholderPassword1", password_confirmation: "PlaceholderPassword1" } }
+    assert_response :redirect
+  end
+
+  test "should not get edit when logged out" do
+    get edit_user_registration_path
+    assert_response :redirect
+  end
+
+  test "should get edit" do
+    sign_in users(:two)
+
+    get edit_user_registration_path
+    assert_response :success
+  end
+
+  test "should not update without password" do
+    sign_in users(:two)
+
+    patch user_registration_path, params: { user: { username: "twoupdated" } }
+    assert_response :unprocessable_entity
+  end
+
+  test "should not update with invalid password" do
+    sign_in users(:two)
+
+    patch user_registration_path, params: { user: { username: "twoupdated", current_password: "Placeholder1" } }
+    assert_response :unprocessable_entity
+  end
+
+  test "should not update invalid username" do
+    sign_in users(:two)
+
+    patch user_registration_path, params: { user: { username: "ab", current_password: "PlaceholderPassword1" } }
+    assert_response :unprocessable_entity
+  end
+
+  test "update username" do
+    sign_in users(:two)
+
+    patch user_registration_path, params: { user: { username: "twoupdated", current_password: "PlaceholderPassword1" } }
+    assert_response :redirect
+  end
+
+  test "update password" do
+    sign_in users(:two)
+
+    patch user_registration_path, params: { user: { password: "Placeholder1", password_confirmation: "Placeholder1", current_password: "PlaceholderPassword1" } }
+    assert_response :redirect
+  end
+
+  test "unconfirmed user can not log in" do
+    post user_session_path, params: { user: { email: "created@shadowcrafter.org", password: "PlaceholderPassword1" } }
+    assert_response :unprocessable_entity
+  end
+
+  test "user can log in" do
+    post user_session_path, params: { user: { email: "confirmed@shadowcrafter.org", password: "PlaceholderPassword1" } }
+    assert_response :redirect
+  end
+
+  test "user can logout" do
+    delete destroy_user_session_path
+    assert_response :redirect
+  end
+end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.new username: "abc", email: "info@shadowcrafter.org", password: "PlaceholderPassword1", password_confirmation: "PlaceholderPassword1"
+  end
+
+  test "user is valid" do
+    assert @user.valid?
+  end
+
+  test "users with invalid usernames are not valid" do
+    @user.username = "abc."
+    assert_not @user.valid?
+
+    @user.username = "ab"
+    assert_not @user.valid?
+  end
+
+  test "password need to match" do
+    @user.password_confirmation = "Placeholder"
+    assert_not @user.valid?
+  end
+
+  test "password is matched correctly" do
+    assert @user.valid_password? "PlaceholderPassword1"
+  end
+
+  test "user must have unique username" do
+    @user.username = "one"
+    assert_not @user.valid?
+  end
+
+  test "user must have unique email" do
+    @user.email = "email1@shadowcrafter.org"
+    assert_not @user.valid?
+  end
+
+  test "user must have valid email" do
+    @user.email = "email"
+    assert_not @user.valid?
+  end
+
+  test "user should have user role" do
+    assert_equal "user", @user.role
+  end
 end
