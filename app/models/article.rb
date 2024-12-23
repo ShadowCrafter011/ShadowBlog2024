4eea0633d14fda0c75d6773bab54a22501d9fdab
Lukas <lkoe@bluewin.ch>
2024-12-23 13:13:03 +0000

    Added better slug options for articles in case title is already taken

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,17 @@ class Article < ApplicationRecord
   belongs_to :user
 
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :slug_candidates, use: :slugged
 
   has_rich_text :body
+
+  def slug_candidates
+    [ :title, [ :title, :creator_slug ] ] + 10.times.map { |exp| [ :title, Random.rand(10 ** (exp + 5)) ] }
+  end
+
+  def creator_slug
+    "by-#{user.slug}"
+  end
 
   def user_can_edit?(user)
     user.present? && (user.owner? || user == self.user)
