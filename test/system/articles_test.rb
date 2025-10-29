2708b1771f1ab244e0e426ea95ba00e32958829b
Lukas <lkoe@bluewin.ch>
2025-10-29 17:55:49 +0000

    Hopefully fixed gcs

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -9,7 +9,7 @@ class ArticlesTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit articles_url
-    assert_selector "h1", text: "Articles"
+    assert_selector "h1", text: "ShadowBlog"
   end
 
   test "should create article" do
